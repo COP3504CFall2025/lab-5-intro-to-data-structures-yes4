e04ab4e116e5eef707f95e9a4409bc5bbe561c8b
prateekbandi <pratacademics@gmail.com>
2025-11-11 17:08:22 -0500

    idk anymore

--- ABQ.hpp
+++ ABQ.hpp
@@ -107,7 +107,7 @@ public:
         }
         curr_size_--;
 
-        while (curr_size_ > 0 && capacity_ >= curr_size_ * 2) {
+        if (curr_size_ > 0 && curr_size_ * 4 <= capacity_) {
             size_t new_cap = capacity_ / 2;
             if (new_cap < 1) new_cap = 1;
             T* new_array = new T[new_cap];
